cb19636ce21614a40767e0b91e6df3461c1d5fe1
Adrian Feijó Fagundes <adrianfeijofagundes@gmail.com>
2025-03-12 19:45:53 -0300

    refactor: reinitialize project

--- src/database/connection.ts
+++
@@ -1,9 +0,0 @@
-import * as sqlite3 from "sqlite3";
-import { open } from "sqlite";
-
-export const connectDB = async () => {
-  return open({
-    filename: "./database.db",
-    driver: sqlite3.Database
-  });
-};
